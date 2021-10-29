afba81a63bad9b0ffb1cc159b3009e560d617ba2
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-29 14:09:29 +0200

    Declare variables by each of 3 approaches

---
+++ section-02/variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// Declare and assign separately
+	var firstNumber int
+	firstNumber = 2
+
+	// Declare and assign value
+	var secondNumber = 5
+
+	// declare and assign with := operator
+	subtraction := 7
+}
