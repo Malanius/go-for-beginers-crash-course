1ddbf225ba03cf26e4aedb88080f4305e9cb4642
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-29 14:22:19 +0200

    Create guess the number game with variables

--- section-02/variables/main.go
+++ section-02/variables/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Constant
+const promt = "and press ENTER when ready..."
+
 func main() {
 	// Declare and assign separately
 	var firstNumber int
@@ -10,4 +19,31 @@ func main() {
 
 	// declare and assign with := operator
 	subtraction := 7
+
+	// Without assigment, has default value, for int = 0
+	var answer int
+
+	reader:= bufio.NewReader(os.Stdin)
+
+	fmt.Println("Gues the Number Game")
+	fmt.Println("--------------------")
+	fmt.Println()
+
+	fmt.Println("Think of a number between 1 and 10", promt)
+	reader.ReadString('\n')
+
+	fmt.Println("Multiply your number by", firstNumber, promt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multipy the result by", secondNumber, promt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", promt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, promt)
+	reader.ReadString('\n')
+
+	answer = firstNumber * secondNumber - subtraction
+	fmt.Println("The answer is", answer)
 }
