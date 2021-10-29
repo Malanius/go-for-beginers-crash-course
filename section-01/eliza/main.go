f9ea6fbd13e100f9582fe4b07f5409208470073b
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-29 11:20:53 +0200

    Read string from stdin

--- section-01/eliza/main.go
+++ section-01/eliza/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"eliza/doctor"
 	"fmt"
+	"os"
 )
 
-func main(){
-	var whatToSay string
-	whatToSay = doctor.Intro()
+func main() {
+	reader := bufio.NewReader(os.Stdin)
 
+	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
+
+	userInput, _ := reader.ReadString('\n')
+	fmt.Println(userInput)
 }
