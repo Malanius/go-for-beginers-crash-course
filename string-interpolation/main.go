059b172b9c325c78dcba806633eebd67bc7912cd
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-29 16:40:01 +0200

    Create project for string interpolation

---
+++ string-interpolation/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	userName := readString("That is your name?")
+	age := readInt("How old are you?")
+	fmt.Println("Your name is", userName, ". And you are", age, "years old.")
+}
+
+func promt() {
+	fmt.Print("-> ")
+}
+
+func readString(s string) string {
+	for {
+		fmt.Println(s)
+		promt()
+		userinput, _ := reader.ReadString('\n')
+		userinput = strings.ReplaceAll(userinput, "\r\n", "")
+		userinput = strings.ReplaceAll(userinput, "\n", "")
+
+		if userinput == "" {
+			fmt.Print("Please enter a value!")
+		} else {
+			return userinput
+		}
+	}
+}
+
+func readInt(s string) int {
+	for {
+		fmt.Println(s)
+		promt()
+		userinput, _ := reader.ReadString('\n')
+		userinput = strings.ReplaceAll(userinput, "\r\n", "")
+		userinput = strings.ReplaceAll(userinput, "\n", "")
+		num, err := strconv.Atoi(userinput)
+		if err != nil {
+			fmt.Println("Please enter a whole number!")
+		} else {
+			return num
+		}
+	}
+}
