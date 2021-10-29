853aced4b88cd53168909f8bdd3362c7df4b26b4
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-29 14:42:00 +0200

    Package scoped variables

--- section-02/scope/main.go
+++ section-02/scope/main.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
+var one = "One"
+
 func main() {
-	var one = "One"
 	fmt.Println(one)
 
 	myFunc()
 }
 
 func myFunc() {
-	var one = "the number one"
+	// var one = "the number one"
 	fmt.Println(one)
 }
