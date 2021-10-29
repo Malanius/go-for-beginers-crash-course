266b427839824c19fa10fdf8d74d590d778798e4
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-29 14:53:53 +0200

    Package exports

--- section-02/scope/main.go
+++ section-02/scope/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"scope/packageone"
+)
 
 var one = "One"
 
@@ -8,6 +11,14 @@ func main() {
 	var one = "this is block level variable"
 	fmt.Println(one)
 	myFunc()
+
+	newString := packageone.PublicVar
+	fmt.Println("From packageone:", newString)
+	packageone.Exported()
+
+	// No access as lowercase named things are private (seriously, wtf?)
+	// secondString := packageone.privateVar
+	// packageone.notExported()
 }
 
 func myFunc() {
